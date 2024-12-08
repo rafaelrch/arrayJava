883fe6024ef23f39f8e97e5e43b68812edba78dd
Rafael Rocha <rafaelrocha250304@gmail.com>
2024-12-08 17:13:21 -0300

    exercicio 4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -77,6 +78,53 @@ public class Main {
 
         System.out.printf("\nSOMA = %.2f\n", soma);
         System.out.printf("MEDIA = %.2f\n", media);
+
+        System.out.println("-------------------------------");
+        // Exercicio 4 | Alturas
+
+        int nPessoas, nMenores;
+        double alturaTotal, alturaMedia, porcentagem;
+
+
+        System.out.printf("Quantas pessoas serão digitadas: ");
+        nPessoas = sc.nextInt();
+
+        String[] nomes = new String[nPessoas];
+        int[] idades = new int[nPessoas];
+        double[] alturas = new double[nPessoas];
+
+        for(int i = 0; i < nPessoas; i++){
+            System.out.println("Dados da " + (i + 1) + "a pessoa:");
+            System.out.printf("Nome: ");
+            nomes[i] = sc.next();
+            System.out.printf("Idade: ");
+            idades[i] = sc.nextInt();
+            System.out.printf("Altura: ");
+            alturas[i] = sc.nextDouble();
+        }
+
+        nMenores = 0;
+        alturaTotal = 0;
+
+        for(int i = 0; i < nPessoas; i++){
+            if (idades[i] < 16){
+                nMenores++;
+            }
+            alturaTotal += alturas[i];
+        }
+
+
+        alturaMedia = alturaTotal / nPessoas;
+        porcentagem = ((double)nMenores / nPessoas) * 100.0;
+
+        System.out.printf("Altura media: %.2f\n", alturaMedia);
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", porcentagem);
+
+        for(int i = 0; i < nPessoas; i++){
+            if(idades[i] < 16){
+                System.out.printf("%s\n", nomes[i]);
+            }
+        }
 
 
 
